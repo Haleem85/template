cbde17afd316e8090543fff0016579de880acc19
Haleem85 <aassalahi@gmail.com>
2022-03-12 03:29:21 +0300

    add portfolio

--- src/TEMPLATE/components/App.js
+++ src/TEMPLATE/components/App.js
@@ -2,12 +2,16 @@ import React from "react";
 import { Navbar } from "./Navbar/index";
 import Home from "./Home";
 import Work from "./Work";
+import {Portfolio} from "./Portfolio";
 
 const App = () => {
   return (
     <div>
       <div>
         <Navbar />
+      </div>
+      <div>
+        <Portfolio />
       </div>
       <div>
         <Work />
