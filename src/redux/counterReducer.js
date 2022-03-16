325a3b5d1e5460d28375d8993e115eb2471cd71b
Haleem85 <aassalahi@gmail.com>
2022-03-16 03:53:28 +0300

    reupload

--- src/redux/counterReducer.js
+++ src/redux/counterReducer.js
@@ -1,7 +1,15 @@
-const counterReducer = (state, action) => {
-  if (action.type) {
-    return state;
+import { INCREMENT, DECREMENT } from "./actions/types";
+const counterReducer = (state = { count: 0 }, action) => {
+  switch (action.type) {
+    case INCREMENT:
+      return state.count + 1;
+
+    case DECREMENT:
+      return state.count - 1;
+
+    default:
+      return state;
   }
 };
 
-export default reducer;
+export default counterReducer;
