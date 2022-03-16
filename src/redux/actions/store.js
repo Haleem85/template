a880397703a07d382e48eb9d5b88ab6ee20bf2d6
Haleem85 <aassalahi@gmail.com>
2022-03-16 03:17:22 +0300

    reupload

--- src/redux/actions/store.js
+++
@@ -1,8 +0,0 @@
-import { createStore } from "redux";
-import rotateReducer from "./rotateReducer";
-
-const  configureStore = (state = { rotating: true })=> {
- return createStore(rotateReducer,state);
-};
-
- export default configureStore;
